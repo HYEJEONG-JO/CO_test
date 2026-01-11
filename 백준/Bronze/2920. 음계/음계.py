f6779944fb387337447f870ab5fc0b2dc9970fb4
HYEJEONG-JO <jhjmain27@gmail.com>
2026-01-11 19:23:40 +0900

    [Bronze II] Title: 음계, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,9 @@
+a = list(map(int, input().split()))
+
+# ascending, descending 체크
+if a == [1, 2, 3, 4, 5, 6, 7, 8]:
+    print("ascending")
+elif a == [8, 7, 6, 5, 4, 3, 2, 1]:
+    print("descending")
+else:
+    print("mixed")
