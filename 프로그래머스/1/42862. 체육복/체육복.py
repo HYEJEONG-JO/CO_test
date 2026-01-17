172b9cb6fc50bb03b5155f206e50361b67316e74
HYEJEONG-JO <jhjmain27@gmail.com>
2026-01-17 23:10:36 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 9.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,16 @@
+def solution(n, lost, reserve):
+    
+    check_lost = set(lost) - set(reserve)
+    check_reserve = set(reserve) - set(lost)
+    check_lost_len = len(check_lost)
+
+    for student in check_lost:
+        if student - 1 in check_reserve:
+            check_reserve.remove(student - 1)
+            check_lost_len -= 1
+            
+        elif student + 1 in check_reserve:
+            check_reserve.remove(student + 1)
+            check_lost_len -= 1
+
+    return n - check_lost_len
