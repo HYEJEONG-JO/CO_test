5fb3fe54ba10e477fcfd00eafc2a602aa886b0fb
HYEJEONG-JO <jhjmain27@gmail.com>
2026-03-07 15:08:39 +0900

    [level 3] Title: 네트워크, Time: 0.50 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,15 +1,15 @@
 def solution(n, computers):
-    visited = [False] * n
+    visit = [False] * n
     count = 0
 
     def dfs(cur):
-        visited[cur] = True
+        visit[cur] = True
         for nxt in range(n):
-            if computers[cur][nxt] == 1 and not visited[nxt]:
+            if computers[cur][nxt] == 1 and not visit[nxt]:
                 dfs(nxt)
 
     for i in range(n):
-        if not visited[i]:
+        if not visit[i]:
             dfs(i)
             count += 1
 
